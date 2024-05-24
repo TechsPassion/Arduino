a2c337c7315b1e5239a200c23f52cdcac1f0dac5
TechsPassion <Support@TechsPassion.com>
2024-05-24 01:21:26 -0500

    Create Using_Joystick_To_Control_Servo.ino

---
+++ Using_Joystick_To_Control_Servo.ino
@@ -0,0 +1,29 @@
+// Define the connections for pins X, Y, and SW
+#include <Servo.h> // Include the Servo library
+Servo servo; // Create a servo object
+const int X = A0, Y = A1, SW = 2;
+
+void setup() {
+  servo.attach(11); // Attach the servo to pin 11
+  // Set pins X, Y, and SW to input mode
+  pinMode(X, INPUT);
+  pinMode(Y, INPUT);
+  pinMode(SW, INPUT_PULLUP);  // Use internal pull-up resistor
+  // Initialize serial communication
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Output analog readings of pin X
+  Serial.print("X: ");
+  servo.write(analogRead(X)/6); // Set servo angle to X degrees
+  Serial.print(analogRead(X)/6); // Print the value being sent to the servo
+  // Output analog readings of pin Y
+  Serial.print("\tY: ");
+  Serial.print(analogRead(Y));
+  // Output digital readings of pin SW (button)
+  Serial.print("\tButton: ");
+  Serial.println(digitalRead(SW));
+  // Delay for 10 milliseconds
+  delay(10);
+}
